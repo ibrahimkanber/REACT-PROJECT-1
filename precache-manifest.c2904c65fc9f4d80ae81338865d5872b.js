34adf102802308f2ce09b4209584b07ecd26f77c
ibrahimKanber <ikanber45@gmail.com>
2020-10-05 23:38:18 +0200

    Updates

---
+++ precache-manifest.c2904c65fc9f4d80ae81338865d5872b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ebd55301caf1c19e9f12f49b869855d",
+    "url": "/ibrahimkanber/React/index.html"
+  },
+  {
+    "revision": "0977941f97ba12e8cdde",
+    "url": "/ibrahimkanber/React/static/css/2.dc17f35a.chunk.css"
+  },
+  {
+    "revision": "0977941f97ba12e8cdde",
+    "url": "/ibrahimkanber/React/static/js/2.f8cf596d.chunk.js"
+  },
+  {
+    "revision": "3a5359d431b8190dc2f36e4703f39768",
+    "url": "/ibrahimkanber/React/static/js/2.f8cf596d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eee8d9db258129fd5cfb",
+    "url": "/ibrahimkanber/React/static/js/main.6f7b83a5.chunk.js"
+  },
+  {
+    "revision": "997b139d1c1f49686871",
+    "url": "/ibrahimkanber/React/static/js/runtime-main.16d4715b.js"
+  }
+]);
